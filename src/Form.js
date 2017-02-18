e439a101a23b7f90576b9230f98d40be7d589a5e
Dmitry Rets <dmitryrets@gmail.com>
2017-02-18 16:53:30 +0100

    Create LoginForm UI.

--- src/Form.js
+++ src/Form.js
@@ -8,12 +8,17 @@ import {
 } from 'react-native'
 import { Field, reduxForm } from 'redux-form/immutable'
 
-const submit = values => {
+const submit = (values) => {
   console.log('submitting immutable form', values.toJS())
 }
 
-const renderInput = ({ input: { onChange, ...restInput }}) => {
-  return <TextInput style={styles.input} onChangeText={onChange} {...restInput} />
+const renderInput = ({ input: { onChange, ...restInput }, secureTextEntry}) => {
+  return <TextInput
+    style={styles.input}
+    onChangeText={onChange}
+    secureTextEntry={Boolean(secureTextEntry)}
+    {...restInput}
+  />
 }
 
 const Form = props => {
@@ -21,8 +26,21 @@ const Form = props => {
 
   return (
     <View style={styles.container}>
-      <Text>Email:</Text>
-      <Field name="email" component={renderInput} />
+      <View>
+        <Text style={styles.title}>Heutagogy</Text>
+      </View>
+      <View>
+        <Text style={styles.label}>Username:</Text>
+        <Field name="username" component={renderInput} />
+      </View>
+      <View>
+        <Text style={styles.label}>Password:</Text>
+        <Field secureTextEntry={true} name="password" component={renderInput} />
+      </View>
+      <View>
+        <Text style={styles.label}>Server address:</Text>
+        <Field name="server-address" component={renderInput} />
+      </View>
       <TouchableOpacity onPress={handleSubmit(submit)}>
         <Text style={styles.button}>Submit</Text>
       </TouchableOpacity>
@@ -30,27 +48,34 @@ const Form = props => {
   )
 }
 
-export default reduxForm({
-  form: 'immutable'
-})(Form)
+export default reduxForm({ form: 'immutable' })(Form)
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+  },
+  label: {
+    fontSize: 20,
+  },
   button: {
     backgroundColor: 'blue',
     color: 'white',
-    height: 30,
-    lineHeight: 30,
-    marginTop: 10,
     textAlign: 'center',
-    width: 250
+    fontSize: 20,
+    padding: 10,
+    minWidth: 250,
   },
   container: {
-
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
   },
   input: {
     borderColor: 'black',
+    fontSize: 20,
     borderWidth: 1,
-    height: 37,
-    width: 250
+    height: 50,
+    minWidth: 250
   }
 })
