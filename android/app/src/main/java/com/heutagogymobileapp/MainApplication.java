92d83676ce79617414ce9451d6a4c5633d0e29e4
Dmitry Rets <dmitryrets@gmail.com>
2017-02-19 15:17:56 +0100

    Linking react-native-vector-icons and react-native-swiss-knife.

--- android/app/src/main/java/com/heutagogymobileapp/MainApplication.java
+++ android/app/src/main/java/com/heutagogymobileapp/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.alinz.rnsk.RNSKPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new VectorIconsPackage(),
+          new RNSKPackage(),
           new SharePackage()
       );
     }
